8dc73996f6cc8d20c5cd9e803ef7c22e0b5592c6
zhangxiao <zx@166zx.com>
2018-12-13 15:01:56 +0800

    添加ts模版的判断

--- index.js
+++ index.js
@@ -7,9 +7,10 @@
 var program = require('commander');
 const download = require('download-git-repo')
 const chalk = require('chalk')
+var prompt = require('prompt');
 
 program
-  .version(require('./package').version,'-v, --version')
+  .version(require('./package').version, '-v, --version')
   .option('-C, --chdir <path>', 'change the working directory')
   .option('-c, --config <path>', 'set config path. defaults to ./deploy.conf')
   .option('-T, --no-tests', 'ignore test hook');
@@ -17,15 +18,43 @@ program
 program
   .command('init project [env]')
   .description('run setup commands for all envs')
-  .option("-s, --setup_mode [mode]", "Which setup mode to use")
-  .action(function(env, options){
+  // .option("-s, --setup_mode [mode]", "Which setup mode to use")
+  .action(function (env, options) {
+
+    //
+    // Start the prompt
+    //
+    prompt.start();
+
+    var property = {
+      name: 'yesno',
+      message: '是否使用typescript,输入yes 或 no',
+      validator: /yes*|no?/,
+      // validator: /y[es]*|n[o]?/,
+      warning: '请输入 yes 或 no',
+      default: 'no'
+    };
+
+    //
+    // Get the simple yes or no property
+    //
+    prompt.get(property, function (err, result) {
+      //
+      // Log the results.
+      //
+      // console.log('Command-line input received:');
+      var init = require('./init.js')
+      if(result.yesno == 'yes'){
+        init.init(true)
+      }else{
+        init.init(false)
+      }
+    });
     var mode = options.setup_mode || "normal";
     env = env || 'all';
     // console.log('setup for %s env(s) with %s mode', env, mode);
-    var init = require('./init.js')
     // console.log(init);
-    
-    init.init()
+
     // download('https://github.com:zlj002/ahamgr_template#master','"./"/asd', { clone: true }, (err) => {
     //     if (err) {
     //       console.log(chalk.red(err))
@@ -40,9 +69,9 @@ program
   .alias('ex')
   .description('execute the given remote cmd')
   .option("-e, --exec_mode <mode>", "Which exec mode to use")
-  .action(function(cmd, options){
+  .action(function (cmd, options) {
     console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-  }).on('--help', function() {
+  }).on('--help', function () {
     console.log('');
     console.log('Examples:');
     console.log('');
@@ -52,7 +81,7 @@ program
 
 program
   .command('*')
-  .action(function(env){
+  .action(function (env) {
     console.log('deploying "%s"', env);
   });
 
--- init.js
+++ init.js
@@ -2,24 +2,6 @@
 var fs = require('fs');
 var path = require('path');
 
-var config = {};
-process.argv.slice(2).forEach(function (item) {
-    switch (item) {
-        case "-j":
-            config.jquery = true;
-            break;
-        case "-s":
-            config.swiper = true;
-            break;
-        case "-v":
-            config.vue = true;
-            break;
-        case "-b":
-            config.bootstrap = true;
-            break;
-    }
-});
-
 var copyFile = function (srcPath, tarPath, cb) {
     var rs = fs.createReadStream(srcPath)
     rs.on('error', function (err) {
@@ -43,10 +25,16 @@ var copyFile = function (srcPath, tarPath, cb) {
     rs.pipe(ws)
 }
 var copyFolder = function (srcDir, tarDir, cb) {
+    fs.exists(srcDir, function (exists) {
+        if (!exists) {
+            cb && cb("目录不存在")
+            return
+        }
+    })
     fs.readdir(srcDir, function (err, files) {
         var count = 0
         var checkEnd = function () {
-            ++count == files && files.length && cb && cb()
+            ++count == files && files.length && cb && cb(err)
         }
 
         if (err) {
@@ -60,7 +48,7 @@ var copyFolder = function (srcDir, tarDir, cb) {
 
             fs.stat(srcPath, function (err, stats) {
                 if (stats.isDirectory()) {
-                    console.log('mkdir', tarPath)
+                    // console.log('mkdir', tarPath)
                     fs.mkdir(tarPath, function (err) {
                         if (err) {
                             console.log('复制模版出错！请查看以下出错信息！');
@@ -78,32 +66,39 @@ var copyFolder = function (srcDir, tarDir, cb) {
             })
         })
 
-        //为空时直接回调
         files.length === 0 && cb && cb()
     })
-    cb && cb()
+    cb && cb(true)
 }
-exports.init = function () {
-    var PATH = ".";
-    // if (process.argv.length == 2) {
-    //     copyTemplate("index_simple.html", PATH + '/index.html');
-    // } else {
-    //     copyTemplate("index.html", PATH + '/index.html');
-    // }
-
-    // copyTemplate("package.json", PATH + '/package.json');
-    var from = path.join(__dirname, 'templates');
+var msg = function () {
+    console.log("项目模版创建成功！");
+    console.log("");
+    console.log("执行 npm install 下载所有依赖");
+    console.log("执行 npm run dev 启动node服务器");
+}
+exports.init = function (boolean) {
+    var js = path.join(__dirname, 'templates_js');
+    var ts = path.join(__dirname, 'templates_ts');
     console.log("开始复制项目模版！请耐心等待！");
+    console.log("");
+    if (boolean) {
+        copyFolder(ts, './', function (res) {
+            if (res == true) {
+                msg()
+            } else {
+                console.log("创建项目失败，请联系作者！");
+            }
+        })
 
-    copyFolder(from, './', function (err) {
-        if (err) {
-            return
-        } else {
-            console.log("项目模版创建成功！");
-            console.log("执行 npm install 下载所有依赖");
-            console.log("执行 npm run dev 启动node服务器");
-        }
-        //continue
-    })
+    } else {
+        copyFolder(js, './', function (res) {
+            if (res == true) {
+                msg()
+            } else {
+                console.log("创建项目失败，请联系作者！");
+            }
+        })
+
+    }
 
 }
